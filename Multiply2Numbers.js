3e5c614d7a52acdd9b5ea33cdfb7b36a8be0fa3b
CharlesCreativeContent <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-11 16:32:26 -0400

    Create Multiply2Numbers.js
    First exercise

---
+++ Multiply2Numbers.js
@@ -0,0 +1,12 @@
+/*
+The code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+*/
+
+//Answer: Function was missing a return method//
+function multiply(a, b){
+  return a * b
+}
