e06037c8d3694252b407394ac6b6f199166e53f5
CharlesCreativeContent <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-11 16:43:37 -0400

    Even_Or_Odd.js
    2nd exercise.

---
+++ Even_Or_Odd.js
@@ -0,0 +1,8 @@
+ /*
+Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+//Answer//
+const even_or_odd = number => { 
+return (number%2 === 0 ? 'Even' : 'Odd')
+}
