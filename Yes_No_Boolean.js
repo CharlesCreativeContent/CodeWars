dadcf82162b6cd3fa9e0db1b68662749ae7e03e0
CharlesCreativeContent <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-12 13:05:16 -0400

    Returns Boolean as Yes or No
    8th exercise

---
+++ Yes_No_Boolean.js
@@ -0,0 +1,10 @@
+/*
+Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+*/
+
+//Answer//
+
+
+function boolToWord( bool ){
+const jay = bool === true ? 'Yes' : 'No';
+return jay;} 
