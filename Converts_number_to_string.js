6715ffcb0b4754b2f70bcba5865a6dbaf983aad9
CharlesCreativeContent <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-12 13:10:36 -0400

    Accepts a number and returns a string
    const numberToString = num => num.toString();

---
+++ Converts_number_to_string.js
@@ -0,0 +1,13 @@
+/*
+We need a function that can transform a number into a string.
+
+What ways of achieving this do you know?
+
+Examples:
+number_to_string(123) /* returns '123'*/
+number_to_string(999) /* returns '999'*/
+*/
+
+//Answer//
+
+const numberToString = num => num.toString();
