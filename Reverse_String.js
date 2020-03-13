fbfab3100c61dd25265b8d97fd36b6486fc501d1
CharlesCreativeContent <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-13 10:43:45 -0400

    Reverses a string
    11th exercise

---
+++ Reverse_String.js
@@ -0,0 +1,10 @@
+/*
+Complete the solution so that it reverses the string value passed into it.
+solution('world'); // returns 'dlrow'
+*/
+
+//Answer//
+
+function solution(str){
+return str.split('').reverse().join('');
+}
