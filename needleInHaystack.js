1d2aa37bb6a69cc7f0ea16c0fa77ddbe07d1d99c
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-14 00:22:01 -0400

    Finding the Index of a String
    16th exercise

---
+++ needleInHaystack.js
@@ -0,0 +1,18 @@
+/*
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+findNeedle(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+should return "found the needle at position 5"
+*/
+
+//Answer//
+
+function findNeedle(haystack) {
+return 'found the needle at position ' + haystack.indexOf('needle')
+}
