f13bc2afe77ec4c7c4619f3f61e99950d6688378
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-15 01:26:27 -0400

    Converts DNA sequence to RNA
    23rd exercise

---
+++ dnaToRna.js
@@ -0,0 +1,21 @@
+/*
+Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems.
+It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells.
+RNA differs slightly from DNA its chemical structure and contains no Thymine.
+In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+DNAtoRNA("GCAT") returns ("GCAU")
+The input string can be of arbitrary length - in particular, it may be empty.
+All input is guaranteed to be valid, i.e. each input string will only ever consist of 'G', 'C', 'A' and/or 'T'.
+*/
+
+//Answer//
+
+function DNAtoRNA(dna) {
+return dna.replace(/T/g,'U')
+}
