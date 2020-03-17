21e11e8f3da8df6bcab71da1d65d869754877d39
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-16 23:30:10 -0400

    Function that multiples by 8 or 9
    29th exercise

---
+++ multiply8Or9.js
@@ -0,0 +1,10 @@
+/*
+This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+*/
+
+//Answer//
+
+function simpleMultiplication(number) {
+    // your code........
+    return (number%2===0 ? number*8: number*9)
+}
