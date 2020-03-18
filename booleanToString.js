870b85436defed4aab25fbac855842fb033866bd
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-18 17:07:15 -0400

    Converts Boolean to String
    37th exercise

---
+++ booleanToString.js
@@ -0,0 +1,20 @@
+/*
+In this programming exercise, you're going to learn about functions, boolean (true/false) values, strings, and the if-statement.
+A function is a block of code that takes an input and produces an output. 
+In this example, boolean_to_string is a function whose input is either true or false, 
+and whose output is the string representation of the input, either "true"/"True" or "false"/"False" 
+(check the sample tests about what capitalization to use in a given language).
+
+A common idea we often want to represent in code is the concept of true and false.
+A variable that can either be true or false is called a boolean variable.
+In this example, the input to boolean_to_string (represented by the variable b) is a boolean.
+Lastly, when we want to take one action if a boolean is true, and another if it is false, we use an if-statement.
+
+For this kata, don't worry about edge cases like where unexpected input is passed to the function.
+You'll get to worry about these enough in later exercises.
+*/
+
+//Answer//
+function booleanToString(b){
+return (b===true?'true':'false') //your code here
+}
