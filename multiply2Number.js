92aa686b2cc2c270b803349be80c0d68f8052523
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-19 11:32:01 -0400

    Calculates the total of two numbers multiplied
    43rd exercise

---
+++ multiply2Number.js
@@ -0,0 +1,9 @@
+/*
+This function has to be called multiply and needs to take two numbers as arguments, and has to return the multiplication of the two arguments.
+*/
+
+//Answer//
+
+const multiply = (a, b) => {
+return a*b
+}
