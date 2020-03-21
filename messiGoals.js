0c7173152f4073c5493341a93428a3175126d270
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-20 21:28:53 -0400

    Calculates sum of array
    47th exercise

---
+++ messiGoals.js
@@ -0,0 +1,17 @@
+/*
+Messi goals function
+Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+*/
+
+//Answer//
