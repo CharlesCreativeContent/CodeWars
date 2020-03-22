6f97bbb783a708fd4ba1094d3d7e4b3ff32661f7
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-22 15:09:32 -0400

    Reverse the words in a string
    52nd exercise

---
+++ reverseWords.js
@@ -0,0 +1,14 @@
+/*
+Complete the solution so that it reverses all of the words within the string passed in.
+
+Example:
+
+reverseWords("The greatest victory is that which requires no battle")
+// should return "battle no requires which that is victory greatest The"
+*/
+
+//Answer//
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(' '); // reverse those words
+}
