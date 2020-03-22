d9de5f3c5776cf38f4bb0e9db833111cd556c4d6
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-22 15:05:27 -0400

    Sends a personal message to the right person
    49th exercise

---
+++ personalMessage.js
@@ -0,0 +1,16 @@
+/*
+Personalized greeting
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+*/
+
+//Answer//
+
+function greet (name, owner) {
+return  (name === owner? 'Hello boss':'Hello guest')
+}
