fa84dae10cf9e6848085d6a4c5ab02bef3fec811
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-25 21:14:44 -0400

    Fixed syntax
    60th exercise

---
+++ syntaxDebugging.js
@@ -0,0 +1,9 @@
+/*
+A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+*/
+
+//Answer//
+
+function main (verb, noun) {
+  return verb + noun
+}
