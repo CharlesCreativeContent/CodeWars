1cb423aabfbe0429f8eb1c180cc0b389043c3cb3
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-25 21:13:21 -0400

    Verifies if item is in the array
    59th exercise

---
+++ enumerableMagic.js
@@ -0,0 +1,13 @@
+
+/*
+Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+*/
+
+//Answer//
+
+function include(arr, item){
+  // ...
+  return arr.includes(item)?true:false
+}
+
+}
