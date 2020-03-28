0599509668cdd2df552647daa15e998d66a703a6
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-27 22:29:53 -0400

    Swaps indexes of a two index array
    73rd exercise

---
+++ swapValues.js
@@ -0,0 +1,13 @@
+
+/*
+I would like to be able to pass an array with two elements to my swapValues function to swap the values.
+However it appears that the values aren't changing.
+
+Can you figure out what's wrong here?
+*/
+
+//Answer//
+
+function swapValues(arr) {
+return arr.reverse();
+}
