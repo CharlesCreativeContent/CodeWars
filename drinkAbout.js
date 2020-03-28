420ea60ed479d9f407577a1ab05d2dc22c25db75
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-27 22:28:41 -0400

    Outputs the available drink based on given age
    72nd exercise

---
+++ drinkAbout.js
@@ -0,0 +1,27 @@
+/*
+Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more.
+Examples:
+
+peopleWithAgeDrink(13) === "drink toddy"
+peopleWithAgeDrink(17) === "drink coke"
+peopleWithAgeDrink(18) === "drink beer"
+peopleWithAgeDrink(20) === "drink beer"
+peopleWithAgeDrink(30) === "drink whisky"
+*/
+
+//Answer//
+
+function peopleWithAgeDrink(old) {
+return (old < 14 ? 'drink toddy' : old < 18 ? 'drink coke' : old < 21 ? 'drink beer' : 'drink whisky')
+;} 
