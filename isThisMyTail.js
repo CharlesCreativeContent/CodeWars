f4283ae064987d864cc2cc138b81e0d33cb8d6f5
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-28 02:05:15 -0400

    Verifies if the creatures have the right tail
    77th exercise

---
+++ isThisMyTail.js
@@ -0,0 +1,17 @@
+/*
+Some new animals have arrived at the zoo. The zoo keeper is concerned that perhaps the animals do not have the right tails.
+To help her, you must correct the broken function to make sure that the second argument (tail),
+is the same as the last letter of the first argument (body) - otherwise the tail wouldn't fit!
+
+If the tail is right return true, else return false.
+
+The arguments will always be strings, and normal letters.
+
+For Haskell, body has the type of String and tail has the type of Char. For Go, body has type string and tail has type rune.
+*/
+
+//Answer//
+
+function correctTail(bod, tail) {
+return (bod[bod.length - 1]===tail ? true:false)
+}
