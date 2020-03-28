7be429d13e2fcd867d6da07ccefe458e46aa53a9
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-27 22:27:21 -0400

    Recreates the .endsWith() method
    71st exercise

---
+++ stringEndsWith.js
@@ -0,0 +1,15 @@
+
+/*
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+*/
+
+//Answer//
+
+function solution(str, ending){
+return str.endsWith(ending)
+}
