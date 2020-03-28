42ebe0ff9bb4b4dcced171e5e9a7e7b724441244
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-28 02:03:12 -0400

    Multiplies a number by 50 and then adds 6
    76th exercise

---
+++ fiftyPlusSix.js
@@ -0,0 +1,11 @@
+/*
+Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+Note: in C#, you'll always get the input as a string, so the above applies if the string isn't representing a double value.
+*/
+
+//Answer//
+
+function problem(x){
+return typeof(x)==='string'?'Error':(x*50)+6
+}
