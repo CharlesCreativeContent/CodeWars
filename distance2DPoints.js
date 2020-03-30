e16ad3fde26fa1c55a1d47430969a88495913078
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-30 07:02:32 -0400

    calculates distance between two points
    80th exercise

---
+++ distance2DPoints.js
@@ -0,0 +1,21 @@
+/*
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have x and y attributes (X and Y in C#) attributes.
+
+Write a function calculating distance between Point a and Point b.
+
+Tests round answers to 6 decimal places.
+*/
+
+//Answer//
+
+function distanceBetweenPoints(a, b) {
+  return Math.hypot(a.x - b.x, a.y - b.y);
+}
+//OR//
+function distanceBetweenPoints(a, b) {
+const X = a.x - b.x;
+const Y = a.y - b.y;
+return  Math.sqrt((X*X) + (Y*Y));
+}
