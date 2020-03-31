87243c03da4e5995ed9e8466c8b6502d075f00f1
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-03-31 11:25:59 -0400

    Verifies if our hero survives
    85th exercise

---
+++ surviveOrNot.js
@@ -0,0 +1,11 @@
+/*
+A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
+
+Return True if yes, False otherwise :)
+*/
+
+//Answer//
+
+function hero(bullets, dragons){
+return (dragons<=bullets/2?true:false);
+}
