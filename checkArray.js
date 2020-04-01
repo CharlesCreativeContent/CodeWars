32cb35b7f597760479cb510576efd075230e23a6
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-01 14:49:03 -0400

    Checks if a variable is an element of an array
    93rd exercise

---
+++ checkArray.js
@@ -0,0 +1,17 @@
+/*
+* No Loops Allowed *
+
+You will be given an array (a) and a value (x).
+All you need to do is check whether the provided array contains the value,
+without using a loop.
+
+Array can contain numbers or strings. X can be either.
+Return true if the array contains the value, false if not.
+With strings you will need to account for case.
+*/
+
+//Answer//
+
+function check(a,x){
+return a.indexOf(x)===-1?false:true};
+
