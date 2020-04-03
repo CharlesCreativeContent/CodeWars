ba979afa2736f35c4da669617eea4d9ecdad79c8
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-02 21:29:48 -0400

    Returns an array filled to a specific n
    97th exercise

---
+++ countMonkeys.js
@@ -0,0 +1,18 @@
+/*
+You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+For example:
+
+monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+monkeyCount(1) // --> [1]
+*/
+
+//Answer//
+
+function monkeyCount(n) {
+const M=[];
+for(let i = 1; i <= n; i++) { M.push(i);}
+return M
+}
