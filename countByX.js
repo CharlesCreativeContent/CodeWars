7979ef1676343baf4c5b0d56eecaab0dd182b962
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-03 22:50:27 -0400

    Fills array with n indexes and counts by X
    99th exercise

---
+++ countByX.js
@@ -0,0 +1,20 @@
+/*
+Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array (or list in Python, Haskell or Elixir).
+
+Examples:
+
+countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+countBy(2,5) === [2,4,6,8,10]
+*/
+
+//Answer//
+
+function countBy(x, n) {
+const C=[];
+for(let i = x; i <= x*n; i+=x) { C.push(i);}
+return C
+}
