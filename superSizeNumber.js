25d9d1c629e45fdb881360bbe34b9f280c76f013
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-03 22:53:46 -0400

    Rearranges digits into largest possible number
    101st exercise

---
+++ superSizeNumber.js
@@ -0,0 +1,15 @@
+/*
+Write a function that rearranges an integer into its largest possible value.
+
+superSize(123456) //654321
+superSize(105) // 510
+superSize(12) // 21
+If the argument passed through is single digit or is already the maximum possible integer, your function should simply return it.
+*/
+
+//Answer//
+
+function superSize(num){
+const nos = num.toString();
+return Number(nos.split('').sort().reverse().join(''))
+}
