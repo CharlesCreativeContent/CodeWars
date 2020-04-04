47de02bbefed1d1ebd38a1bfacf478834b4c5293
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-03 22:51:52 -0400

    Fills array with odd under X
    100th exercise

---
+++ countOddUnderX.js
@@ -0,0 +1,12 @@
+/*
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+oddCount(7) //=> 3, i.e [1, 3, 5]
+oddCount(15) //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+*/
+
+//Answer//
+
+function oddCount(n){
+return  Math.floor(n/2)
+}
