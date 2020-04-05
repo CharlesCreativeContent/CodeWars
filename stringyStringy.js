95549a9e31e3acc4b3ea871753e5fa15f48dc52f
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-05 11:46:28 -0400

    Array with set number of alternating 1's and 0's
    112th exercise

---
+++ stringyStringy.js
@@ -0,0 +1,23 @@
+/*
+write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+
+the string should start with a 1.
+
+a string with size 6 should return :'101010'.
+
+with size 4 should return : '1010'.
+
+with size 12 should return : '101010101010'.
+
+The size will always be positive and will only use whole numbers.
+*/
+
+//Answer//
+
+function stringy(size) {
+let A=[]
+for(let i=1;i<size+1;i++){
+A.push(i%2)
+}
+return A.join('')
+}
