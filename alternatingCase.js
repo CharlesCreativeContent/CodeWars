5f3a0c30307c28dc5d93670456f8d23716f3d238
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-06 13:09:15 -0400

    Changes the case of a string
    118th exercise

---
+++ alternatingCase.js
@@ -0,0 +1,24 @@
+/*
+Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+"hello world".toAlternatingCase() === "HELLO WORLD"
+"HELLO WORLD".toAlternatingCase() === "hello world"
+"hello WORLD".toAlternatingCase() === "HELLO world"
+"HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+"12345".toAlternatingCase() === "12345" // Non-alphabetical characters are unaffected
+"1a2b3c4d5e".toAlternatingCase() === "1A2B3C4D5E"
+"String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+As usual, your function/method should be pure, i.e. it should not mutate the original string.
+*/
+
+//Answer//
+
+String.prototype.toAlternatingCase = function () {
+let A = []
+let B = this.split('')
+for(let i=0; i<this.length;i++){
+if (B[i]=== B[i].toLowerCase() && B[i] !== B[i].toUpperCase()) {A.push(B[i].toUpperCase())}
+else {A.push(B[i].toLowerCase())}
+}
+return A.join("")
+}
