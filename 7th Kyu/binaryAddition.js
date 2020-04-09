f2c84db631759edeaa00d0f2b5f0f22e798e8589
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-09 00:54:19 -0400

    Given two numbers. Returns Binary sum
    13th exercise

---
+++ 7th Kyu/binaryAddition.js
@@ -0,0 +1,12 @@
+/*
+Implement a function that adds two numbers together and returns their sum in binary.
+The conversion can be done before, or after the addition.
+
+The binary number returned should be a string.
+*/
+
+//Answer//
+
+function addBinary(a,b) {
+return (a+b).toString(2)
+}
