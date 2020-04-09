9b7612f9a910f13b78052199dc5b5096150e4534
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-09 00:55:57 -0400

    Translates binary numbers
    14th exercise

---
+++ 7th Kyu/onesAndZeros.js
@@ -0,0 +1,23 @@
+/*
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+However, the arrays can have varying lengths, not just limited to 4.
+*/
+
+//Answer//
+
+const binaryArrayToNumber = arr => {
+return parseInt(arr.join(''),2)
+};
