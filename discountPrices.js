170d144110cb2891c7c970085c9c4d7bcdb36742
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-11 01:35:52 -0400

    Finds store privrdq
    24th exercise

---
+++ discountPrices.js
@@ -0,0 +1,19 @@
+/*
+Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+Task
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Examples
+minMax([1,2,3,4,5])   == [1,5]
+minMax([2334454,5])   == [5, 2334454]
+minMax([1])           == [1, 1]
+Remarks
+All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+*/
+
+//Answer//
+
+function minMax(arr){
+const L = (a, b) => {return a - b};
+return [arr.sort(L)[0], arr.sort(L)[arr.length-1]]
