635e6e05b02c8febac33f8de4bf347ccc4316d56
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-10 21:39:57 -0400

    Sorts Array
    20th exercise

---
+++ 7th Kyu/sortArray.js
@@ -0,0 +1,10 @@
+/*
+Just a simple sorting usage.
+Create a function that returns the elements of the input-array in a sorted manner.
+*/
+
+//Answer//
+
+function sortme ( names ){
+return names.sort()
+}
