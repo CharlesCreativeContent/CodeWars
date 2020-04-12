3d489a810b6881a38f632ffe9d7beae3c6f5c004
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-12 17:46:50 -0400

    Returns two greatest numbers
    28th exercise

---
+++ 7th Kyu/twoOldestAges.js
@@ -0,0 +1,15 @@
+/*
+The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+The order of the numbers passed in could be any order. The array will always include at least 2 items.
+
+For example:
+
+twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+*/
+
+//Answer//
+
+function twoOldestAges(ages){
+return ages.sort(function(a, b){return a-b}).slice(ages.length-2, ages.length)
+}
