6229c3f87a1529791f3ee763efef9ecd97d97db7
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-13 17:33:42 -0400

    Finds the occurences of an element
    34th exercise

---
+++ occurrencesOfElement.js
@@ -0,0 +1,23 @@
+/*
+Given an array (a list in Python) of integers and an integer n,
+find all occurrences of n in the given array and
+return another array containing all the index positions of n in the given array.
+
+If n is not in the given array, return an empty array [].
+
+Assume that n and all values in the given array will always be integers.
+
+Example:
+
+findAll([6, 9, 3, 4, 3, 82, 11], 3) => [2, 4]
+*/
+
+//Answer//
+
+const findAll = (array, n) => {
+let I = []
+for(let i = 0; i <= array.length-1; i++){
+if (n===array[i]) {I.push(i)}
+}
+return I
+}
