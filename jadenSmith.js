4a9e2f3fbc5e027787963b0d42d2ec81e90edecf
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-15 19:50:30 -0400

    Capitalizes the first letter of every word
    36th exercise

---
+++ jadenSmith.js
@@ -0,0 +1,21 @@
+/*
+Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+
+Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+
+Example:
+
+Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+*/
+
+//Answer//
+
+String.prototype.toJadenCase = function () {
+let S = this.split(' ')
+let Str = [];
+for(let i=0;i<S.length;i++) {
+Str.push(S[i][0].toUpperCase()+S[i].slice(1, S[i].length))
+}
+return Str.join(' ')
+}
