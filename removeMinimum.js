d724070cca8102ef268756a22696d6fdf057ee90
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-15 19:48:52 -0400

    Removes minimum from array
    38th exercise

---
+++ removeMinimum.js
@@ -0,0 +1,25 @@
+/*
+The museum of incredible dull things
+The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+Task
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+
+Examples
+removeSmallest([1,2,3,4,5]) = [2,3,4,5]
+removeSmallest([5,3,2,1,4]) = [5,3,2,4]
+removeSmallest([2,2,1,2,1]) = [2,2,2,1]
+*/
+
+//Answer//
+
+function removeSmallest(numbers) {
+let N = [...numbers];
+if (numbers===[]) {return numbers} 
+else {N.splice(Number(N.indexOf(Math.min(...N))), 1)}
+return N
+}
