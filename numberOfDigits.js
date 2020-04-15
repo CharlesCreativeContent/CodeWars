40f4da0d9eaf3892797797ec7b172d1bbcc76805
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-15 19:46:45 -0400

    Counts number of digits
    40th exercise

---
+++ numberOfDigits.js
@@ -0,0 +1,12 @@
+/*
+Determine the total number of digits in the integer (n>=0) given as input to the function.
+For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+
+All inputs will be valid.
+*/
+
+//Answer//
+
+function digits(n) {
+return String(n).length
+}
