a213f3aefe133bbb0f0cb8c68f290b37becc257d
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-22 09:33:33 -0400

    creates get name objects
    54th exercise

---
+++ 7th Kyu/fixGetNames.js
@@ -0,0 +1,19 @@
+/*
+The following code is not giving the expected results. Can you figure out what the issue is?
+
+The following is an example of data that would be passed in to the function.
+
+var data = [
+  {name: 'Joe', age: 20},
+  {name: 'Bill', age: 30},
+  {name: 'Kate', age: 23}
+]
+
+getNames(data) // should return ['Joe', 'Bill', 'Kate']
+*/
+
+//Answer//
+
+function getNames(data){
+return data.map(item=> item.name);
+}
