145677bbac6e6ed2878081dea2affae11b460bb9
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-23 01:30:13 -0400

    Returns the min and max of an array
    60th exercise

---
+++ 7th Kyu/minMax.js
@@ -0,0 +1,9 @@
+/*
+Implement a function that returns the minimal and the maximal value of a list (in this order).
+/*
+
+//Answer//
+
+function getMinMax(arr){
+return [Math.min(...arr),Math.max(...arr)]
+};
