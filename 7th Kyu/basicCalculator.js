bd7b5b4a0783bfc386c7fe8643418c6bbdc497ea
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-24 23:56:06 -0400

    Does basic calculator functions
    62nd exercise

---
+++ 7th Kyu/basicCalculator.js
@@ -0,0 +1,27 @@
+/*
+Write a function called calculate that takes 3 values. The first and third values are numbers. The second value is a character. If the character is "+" , "-", "*", or "/", the function will return the result of the corresponding mathematical function on the two numbers. If the string is not one of the specified characters, the function should return null (throw an ArgumentException in C#).
+
+calculate(2,"+", 4); //Should return 6
+calculate(6,"-", 1.5); //Should return 4.5
+calculate(-4,"*", 8); //Should return -32
+calculate(49,"/", -7); //Should return -7
+calculate(8,"m", 2); //Should return null
+calculate(4,"/",0) //should return null
+Keep in mind, you cannot divide by zero. If an attempt to divide by zero is made, return null (throw an ArgumentException in C#).
+/*
+
+//Answer//
+
+function calculate(num1, operation, num2) {
+if (num2===0&&operation==='/'){return null}else{
+switch (operation){
+case '+':return num1+num2;
+break;
+case '-':return num1-num2
+break;
+case '*':return num1*num2
+break;
+case '/':return num1/num2;
+default:return null;
+}}
+}
