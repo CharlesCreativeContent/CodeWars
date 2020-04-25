4d499064f313c33706581db137c864677d3a9e00
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-24 23:59:11 -0400

    Makes padded numbers
    65th exercise

---
+++ 7th Kyu/paddedNumbers.js
@@ -0,0 +1,30 @@
+/*
+Complete the solution so that it returns a formatted string. The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+
+Example:
+
+solution(5) // should return "Value is 00005"
+/*
+
+//Answer//
+
+function solution(value){
+switch (String(value).length) {
+case 1:
+return `Value is 0000${value}`;
+break;
+case 2:
+return `Value is 000${value}`;
+break;
+case 3:
+return `Value is 00${value}`;
+break;
+case 4:
+return `Value is 0${value}`;
+break;
+case 0:
+return "Value is 00000";
+break;
+default: `Value is ${value}`;
+}
+}
