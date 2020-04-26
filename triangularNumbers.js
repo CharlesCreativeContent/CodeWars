6e5cf2f6add16b565db8127bde6386dfc355f845
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-26 10:42:12 -0400

    returns nth triangular number
    71st exercise

---
+++ triangularNumbers.js
@@ -0,0 +1,26 @@
+/*
+Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+
+1st (1)   2nd (3)    3rd (6)
+*          **        ***
+           *         **
+                     *
+You need to return the nth triangular number. You should return 0 for out of range values:
+
+  triangular(0)==0,
+  triangular(2)==3,
+  triangular(3)==6,
+  triangular(-10)==0
+/*
+
+//Answer//
+
+function triangular( n ) {
+if(n<=0){return 0}else{
+let A=[]
+for(let i=0;i<=n;i++){
+A.push(i)
+}
+return A.reduce((a,b)=>a+b)
+}
+}
