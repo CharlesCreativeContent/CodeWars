316043c9e84f86517df5c1add0ad36bce79ee7bb
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-28 11:34:06 -0400

    Finds the number that is between the other numbers
    83rd exercise

---
+++ 7th Kyu/findMiddleElement.js
@@ -0,0 +1,23 @@
+/*
+As a part of this Kata, you need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
+
+The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
+For example:
+
+gimme([2, 3, 1]) => 0
+2 is the number that fits between 1 and 3 and the index of 2 in the input array is 0.
+
+Another example (just to make sure it is clear):
+
+gimme([5, 10, 14]) => 1
+10 is the number that fits between 5 and 14 and the index of 10 in the input array is 1.
+*/
+
+//ANSWER//
+
+var gimme = function (inputArray) {
+let A = [...inputArray];
+A.sort((a,b)=>a-b);
+return inputArray.indexOf(A[1])
+};
