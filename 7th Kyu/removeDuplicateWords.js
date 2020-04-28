1f19b18ef252fa7db2d22e8a1393c3a563fc21ab
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-28 11:35:13 -0400

    Removes duplicate words from the sentence
    84th exercise

---
+++ 7th Kyu/removeDuplicateWords.js
@@ -0,0 +1,23 @@
+/*
+Your task is to remove all duplicate words from a string, leaving only single (first) words entries.
+
+Example:
+
+Input:
+
+'alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta'
+
+Output:
+
+'alpha beta gamma delta'In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
+
+More details about factorial can be found here.
+*/
+
+//ANSWER//
+
+function removeDuplicateWords (s) {
+return [...new Set(s.split(' '))].join(' ')
+}
