01bcfc92916c70b6b76ba926caedd1b1b7540801
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-04-28 11:32:21 -0400

    Square functions on a string
    82nd exercise

---
+++ 7th Kyu/squaredStrings.js
@@ -0,0 +1,48 @@
+/*
+You are given a string of n lines, each substring being n characters long: For example:
+
+s = "abcd\nefgh\nijkl\nmnop"
+
+We will study some transformations of this square of strings.
+
+Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+or printed:
+
+vertical mirror   |horizontal mirror   
+abcd --> dcba     |abcd --> mnop 
+efgh     hgfe     |efgh     ijkl 
+ijkl     lkji     |ijkl     efgh 
+mnop     ponm     |mnop     abcd 
+#Task:
+
+Write these two functions
+and
+
+high-order function oper(fct, s) where
+
+fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+#Examples:
+
+s = "abcd\nefgh\nijkl\nmnop"
+oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd"
+Note:
+The form of the parameter fct in oper changes according to the language. You can see each form according to the language in "Sample Tests".
+
+
+*/
+
+//ANSWER//
+
+function vertMirror(strng) {
+return strng.split('\n').map(a=>a.split('').reverse().join('')).join('\n')
+}
+function horMirror(strng) {
+return strng.split('\n').reverse().join('\n')
+}
+function oper(fct, s) {
+return fct===vertMirror?vertMirror(s):horMirror(s);
+}
