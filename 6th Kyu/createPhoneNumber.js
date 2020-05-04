052029e682d3bbae50103536c6745ba8b5ea0132
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-04 08:54:12 -0400

    Creates phone number format
    5th exercise

---
+++ 6th Kyu/createPhoneNumber.js
@@ -0,0 +1,18 @@
+!ans
+
+function createPhoneNumber(numbers){
+  var format = "(xxx) xxx-xxxx";
+  
+  for(var i = 0; i < numbers.length; i++)
+  {
+    format = format.replace('x', numbers[i]);
+  }
+  
+  return format;
+}
+
+//Or//
+
+function createPhoneNumber(numbers){
+  return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`
+}
