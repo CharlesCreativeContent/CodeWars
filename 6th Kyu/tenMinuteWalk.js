0d7eb737bfc512902da4bcaa98ccc59808f6a33c
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-05 07:59:55 -0400

    Decides if array is a ten minute walk
    9th exercise

---
+++ 6th Kyu/tenMinuteWalk.js
@@ -0,0 +1,7 @@
+!ans
+
+function isValidWalk(walk) {
+if (walk.filter(x=>x==='n').length!==walk.filter(x=>x==='s').length||walk.filter(x=>x==='e').length!==walk.filter(x=>x==='w').length||walk.length!==10){
+return false
+}else{return true}
+}
