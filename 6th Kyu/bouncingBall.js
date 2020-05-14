2f51305c93487e8b80d903c67fe54f23564408a3
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-14 07:52:19 -0400

    Counts bounces that are over a certain height
    20th exercise - codewars.com/kata/5544c7a5cb454edb3c000047

---
+++ 6th Kyu/bouncingBall.js
@@ -0,0 +1,39 @@
+/*
+A child is playing with a ball on the nth floor of a tall building. The height of this floor, h, is known.
+
+He drops the ball out of the window. The ball bounces (for example), to two-thirds of its height (a bounce of 0.66).
+
+His mother looks out of a window 1.5 meters from the ground.
+
+How many times will the mother see the ball pass in front of her window (including when it's falling and bouncing?
+
+Three conditions must be met for a valid experiment:
+Float parameter "h" in meters must be greater than 0
+Float parameter "bounce" must be greater than 0 and less than 1
+Float parameter "window" must be less than h.
+If all three conditions above are fulfilled, return a positive integer, otherwise return -1.
+
+Note:
+The ball can only be seen if the height of the rebounding ball is strictly greater than the window parameter.
+
+Example:
+- h = 3, bounce = 0.66, window = 1.5, result is 3
+
+- h = 3, bounce = 1, window = 1.5, result is -1 
+
+(Condition 2) not fulfilled).
+*/
+
+//Answer//
+
+function bouncingBall(h,  bounce,  window) {
+if(h<=0||bounce<=0||bounce>=1||window>=h){return -1}else if (h*bounce<=window){return 1}else{
+let count = 1
+h *= bounce
+while(h>=window){
+count += 2
+h *= bounce
+}
+return count
+}
+}
