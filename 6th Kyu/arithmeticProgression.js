6a9a2ac1b7030b296bf61fbe4f70e51256d13151
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-15 00:12:35 -0400

    Finds missing term in arithmetic progression
    25th exercise - https://www.codewars.com/kata/52de553ebb55d1fca3000371

---
+++ 6th Kyu/arithmeticProgression.js
@@ -0,0 +1,14 @@
+/*
+An Arithmetic Progression is defined as one in which there is a constant difference between the consecutive terms of a given series of numbers. You are provided with consecutive elements of an Arithmetic Progression. There is however one hitch: exactly one term from the original series is missing from the set of numbers which have been given to you. The rest of the given series is the same as the original AP. Find the missing term.
+
+You have to write a function that receives a list, list size will always be at least 3 numbers. The missing term will never be the first or last one.
+
+Example
+findMissing([1, 3, 5, 9, 11]) == 7
+*/
+
+//Answer//
+
+var findMissing = function (list) {  
+return ((list.length+1)*(list[0]+list[list.length-1])/2)-list.reduce((a,b)=>a+b)
+}
