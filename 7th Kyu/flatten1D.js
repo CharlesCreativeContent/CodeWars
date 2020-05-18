f30d7f01ea5248afdde0e1c0604903b98f9b6fbe
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-18 16:21:01 -0400

    Flattens one dimension of a nested array
    112th exercise - https://www.codewars.com/kata/5250a89b1625e5decd000413

---
+++ 7th Kyu/flatten1D.js
@@ -0,0 +1,19 @@
+/*
+You are given two arrays a1 and a2 of strings. Each string is composed with letters from a to z. Let x be any string in the first array and y be any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+If a1 and/or a2 are empty return -1 in each language except in Haskell (F#) where you will return Nothing (None).
+
+#Example:
+
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+*/
+
+//Answer//
+
+var flatten = function (array){
+return array.reduce((a,b)=>a.concat(b),[])
+}
