1fe0e741ad8c121cd5d7c21fb5169c4aa87124e1
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-17 23:58:07 -0400

    Flattens one level of array
    112th exercise - https://www.codewars.com/kata/5250a89b1625e5decd000413

---
+++ 7th Kyu/flatten.js
@@ -0,0 +1,13 @@
+/*
+Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+
+flatten([1,2,3]) // => [1,2,3]
+flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+flatten([[[1,2,3]]]) // => [[1,2,3]]
+*/
+
+//Answer//
+
+var flatten = function (array){
+return array.reduce((a,b)=>a.concat(b),[])
+}
