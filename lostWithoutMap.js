4a101f65d0eba87d62c91979be16b92e5211ecd9
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-19 08:57:50 -0400

    Introduction to using map method
    123rd exercise - https://www.codewars.com/kata/53dc54212259ed3d4f00071c

---
+++ lostWithoutMap.js
@@ -0,0 +1,15 @@
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+*/
+
+//Answer//
+
+function maps(x){
+return x.map(x=>x*2)
+}
