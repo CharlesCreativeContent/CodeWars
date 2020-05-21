1bf34fbe63c0a4e328e657d5b974a676b62cfb12
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-21 15:20:01 -0400

    Returns the word's for numbers as a string
    133rd exercise - codewars.com/kata/58f8a3a27a5c28d92e000144

---
+++ switchItUp.js
@@ -0,0 +1,13 @@
+/*
+When provided with a number between 0-9, return it in words.
+
+Input :: 1
+
+Output :: "One".
+
+If your language supports it, try using a switch statement.
+*/
+
+//Answer//
+
+const switchItUp=(n)=>['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine'][n]
