fe8550e6f3a71222b921fc1442e687c72c7c1774
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-21 15:04:57 -0400

    Grabs the string, returns First batch of numbers
    130th exercise - codewars.com/kata/557cd6882bfa3c8a9f0000c1

---
+++ parseInt.js
@@ -0,0 +1,13 @@
+/*
+Ask a small girl - "How old are you?". She always says strange things... Lets help her!
+
+For correct answer program should return int from 0 to 9.
+
+Assume test input string always valid and may look like "1 year old" or "5 years old", etc.. The first char is number only.
+*/
+
+//Answer
+
+function getAge(inputString){
+return +inputString[0]
+}
