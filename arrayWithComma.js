f5240558f9e211248d4f3e6bb1a7330b4803c8c8
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-23 09:52:37 -0400

    Joins an array with commas in between
    142nd exercise - https://www.codewars.com/kata/56e2f59fb2ed128081001328

---
+++ arrayWithComma.js
@@ -0,0 +1,15 @@
+/*
+Input: Array of elements
+
+["h","o","l","a"]
+
+Output: String with comma delimited elements of the array in th same order.
+
+"h,o,l,a"
+*/
+
+// Answer//
+
+function printArray(array){
+return array.join(',')
+}
