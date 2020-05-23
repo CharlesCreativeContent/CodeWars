719a0019885be630275865726566d36a136781df
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-23 09:54:27 -0400

    Counts up to max from min with given steps
    143rd exercise - https://www.codewars.com/kata/55eca815d0d20962e1000106

---
+++ countUp.js
@@ -0,0 +1,24 @@
+/*
+Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step.
+The first integer is the minimum value, the second is the maximum of the range and the third is the step. (min < max)
+
+Task
+Implement a function named
+
+generateRange(2, 10, 2) // should return array of [2,4,6,8,10]
+generateRange(1, 10, 3) // should return array of [1,4,7,10]
+Note
+min < max
+step > 0
+the range does not HAVE to include max (depending on the step)
+*/
+
+// Answer//
+
+function generateRange(min, max, step){
+let A = []
+for(let i = min; i<=max;i+=step){
+A.push(i)
+}
+return A
+}
