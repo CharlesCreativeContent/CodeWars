8499d54e5576142a2983c38601998640370dee23
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-24 09:39:34 -0400

    Returns a greeting with name and city
    146th exercise - https://www.codewars.com/kata/5302d846be2a9189af0001e4

---
+++ cityHello.js
@@ -0,0 +1,14 @@
+/*
+Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space betweeen each, and the length of the name array in test cases will vary.
+
+Example:
+
+sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')
+This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+*/
+
+// Answer//
+
+function sayHello( n, c, s ) {
+return `Hello, ${n.join(' ')}! Welcome to ${c}, ${s}!`
+}
