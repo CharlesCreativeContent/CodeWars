40e2a1ce456c7b450e75d75dfc4da64d495b203c
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-24 09:38:39 -0400

    Returns five without using number characters
    145th exercise - https://www.codewars.com/kata/59441520102eaa25260000bf

---
+++ returnsFive.js
@@ -0,0 +1,11 @@
+/*
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+*/
+
+// Answer//
+
+function unusualFive() {
+return 'seven'.length
+}
