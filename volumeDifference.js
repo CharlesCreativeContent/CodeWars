1b2d4c26fac8ecab27e11651063a75ee000c10d9
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-25 00:34:53 -0400

    Calculates the difference in volume of two arrays
    151st exercise - https://www.codewars.com/kata/58cb43f4256836ed95000f97

---
+++ volumeDifference.js
@@ -0,0 +1,15 @@
+/*
+In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+Your function will be tested with pre-made examples as well as random ones.
+
+If you can, try writing it in one line of code.
+*/
+
+//Answer//
+
+function findDifference(a, b) {
+return Math.abs(a.reduce((a, b) => a * b)-b.reduce((a, b) => a * b))
+}
