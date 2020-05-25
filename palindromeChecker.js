71f0db02edeccea32b3bff9129e64f5e5caf85ac
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-25 00:36:35 -0400

    Verifies if string is a palindrome
    152nd exercise - https://www.codewars.com/kata/57a5015d72292ddeb8000b31

---
+++ palindromeChecker.js
@@ -0,0 +1,17 @@
+/*
+A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward. This includes capital letters, punctuation, and word dividers.
+
+Implement a function that checks if something is a palindrome.
+
+Examples
+isPalindrome("anna")   ==> true
+isPalindrome("walter") ==> false
+isPalindrome(12321)    ==> true
+isPalindrome(123456)   ==> false
+*/
+
+//Answer//
+
+function isPalindrome (l) {
+if (''+l===(''+l).split('').reverse().join('')){return true} else {return false}
+}
