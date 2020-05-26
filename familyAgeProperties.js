60e8aa7439d7286f27bd2540527b1afe1bb3598f
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-26 00:51:38 -0400

    Finds the max age difference in a Family
    158th exercise - https://www.codewars.com/kata/5720a1cb65a504fdff0003e2

---
+++ familyAgeProperties.js
@@ -0,0 +1,11 @@
+/*
+At the annual family gathering, the family likes to find the oldest living family member’s age and the youngest family member’s age and calculate the difference between them.
+
+You will be given an array of all the family members' ages, in any order. The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0. Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+*/
+
+//Answer//
+
+function differenceInAges(ages){
+return [Math.min(...ages),Math.max(...ages),Math.max(...ages)-Math.min(...ages)]
+}
