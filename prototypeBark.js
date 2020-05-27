d91d72dba33f2978f242437240db0dbd147b417e
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-27 07:31:08 -0400

    creates Bark method on a prototype object
    160th exercise - https://www.codewars.com/kata/54dba07f03e88a4cec000caf

---
+++ prototypeBark.js
@@ -0,0 +1,39 @@
+/*
+Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+snoopy.bark(); // return "Woof"
+scoobydoo.bark(); // undefined
+Use method prototypes to enable all Dogs to bark.
+*/
+
+//Answer//
+
+function Dog (breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+Dog.prototype.bark = function() {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+
+//OR//
+
+function Dog (breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+
+scoobydoo.bark = function() {
+  return "Woof";
+};
