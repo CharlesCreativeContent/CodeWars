94e4ebc450ec3aee6ebb6eecddc61a1e271e5b8b
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-27 07:36:06 -0400

    Returns a list/array of the first n elements
    163rd exercise - https://www.codewars.com/kata/545afd0761aa4c3055001386

---
+++ firstNElements.js
@@ -0,0 +1,9 @@
+/*
+Create a method take that accepts a list/array and a number n, and returns a list/array of the first n elements from the list/array.
+*/
+
+//Answer//
+
+function take(arr, n) {
+return arr.slice(0,n)
+}
