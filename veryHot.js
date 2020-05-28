3c054750fdc6fac2671654db9a514bd05bd99431
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-28 12:28:21 -0400

    Verifies if Hot
    165th exercise - https://www.codewars.com/kata/580a094553bd9ec5d800007d

---
+++ veryHot.js
@@ -0,0 +1,12 @@
+  /*
+  Your job is simple, if (x) squared is more than 1000,
+  return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+
+  X will be either a number or a string. Both are valid.
+  */
+
+  //Answer//
+
+  function apple(x){
+  return Number(x)**2>1000?'It\'s hotter than the sun!!':'Help yourself to a honeycomb Yorkie for the glovebox.'
+  }
