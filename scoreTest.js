d5ab64b09432812445ee9e8aa279023669036fac
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-29 00:46:45 -0400

    Calculates a score for the test
    172nd exercise - https://www.codewars.com/kata/5a3dd29055519e23ec000074

---
+++ scoreTest.js
@@ -0,0 +1,22 @@
+/*
+The first input array contains the correct answers to an exam, like ["a", "a", "b", "d"]. The second one is "answers" array and contains student's answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer(empty string).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0
+*/
+
+//Answer//
+
+function checkExam(a1, a2) {
+let correct = 0
+a2.forEach((x,i)=>{if (x===a1[i]){correct+=4}else if(x===''){correct+=0}else{correct-=1}})
+return correct<0?0:correct
+}
