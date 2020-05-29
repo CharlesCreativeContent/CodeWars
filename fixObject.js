3e4247aec768920d67fe0ba5782409474ec06882
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-29 00:45:59 -0400

    Fixes Object function
    171st exercise - codewars.com/kata/514a7ac1a33775cbb500001e

---
+++ fixObject.js
@@ -0,0 +1,10 @@
+    /*
+    This function should return an object, but it's not doing what's intended. What's wrong?
+    */
+
+    //Answer//
+
+    function mystery() {
+      var results = {sanity: 'Hello'};
+      return results;
+    }
