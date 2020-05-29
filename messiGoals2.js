ccd237b3d1423d943bcdb9332a5fe539dec97ada
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-29 00:45:06 -0400

    Calculates Messi's season goal count
    170th exercise - https://www.codewars.com/kata/55ca77fa094a2af31f00002a

---
+++ messiGoals2.js
@@ -0,0 +1,26 @@
+/*
+Use variables to find the sum of the goals Messi scored in 3 competitions
+
+Information
+Messi goal scoring statistics:
+
+Competition	Goals
+La Liga	43
+Champions League	10
+Copa del Rey	5
+Define
+Create three variables and store the appropriate values using the table above:
+
+laLigaGoals
+championsLeagueGoals
+copaDelReyGoals
+Create a fourth variable named totalGoals (javascript, C#, Java) or total_goals (python, ruby). totalGoals/total_goals should store the sum of all of Messi's goals for this year.
+*/
+
+//Answer//
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = 58
