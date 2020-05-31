ca5f24cc5094595092bae13b7f29453334aecf9b
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-30 21:57:22 -0400

    Fix the Function
    176th exercise - https://www.codewars.com/kata/56200d610758762fb0000002

---
+++ fixFunction.js
@@ -0,0 +1,13 @@
+/*
+Fix the function
+I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+Can you help me fix the function?
+*/
+
+//Answer//
+
+function addFive(num) {
+  var total = num + 5
+  return total
+}
