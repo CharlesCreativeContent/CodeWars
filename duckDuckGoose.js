05199662c509a7e9bffe1da7a8f802a29beb4540
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-30 21:56:08 -0400

    Finds out who is the goose
    178th exercise - https://www.codewars.com/kata/582e0e592029ea10530009ce

---
+++ duckDuckGoose.js
@@ -0,0 +1,21 @@
+/*
+The objective of Duck, duck, goose is to walk in a circle, tapping on each player's head until one is chosen.
+
+Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+Example:
+
+duck_duck_goose([a, b, c, d], 1) should return a.name
+duck_duck_goose([a, b, c, d], 5) should return a.name
+duck_duck_goose([a, b, c, d], 4) should return d.name
+// PHP only
+duck_duck_goose([$a, $b, $c, $d], 1); // => $a["name"]
+duck_duck_goose([$a, $b, $c, $d], 5); // => $a["name"]
+duck_duck_goose([$a, $b, $c, $d], 4); // => $d["name"]
+*/
+
+//Answer//
+
+function duckDuckGoose(players, goose) {
+return players[ (goose-1)%(players.length) ].name
+}
