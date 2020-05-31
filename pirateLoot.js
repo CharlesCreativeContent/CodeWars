fcffce66f89ab5b5c4a5341b5a01d8edfae4b1b8
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-05-31 05:18:32 -0400

    Verifies if ship is worth looting
    182nd exercise - codewars.com/kata/54fe05c4762e2e3047000add

---
+++ pirateLoot.js
@@ -0,0 +1,36 @@
+/*
+You are a leader of a small pirate crew. And you have a plan. With the help of OOP you wish to make a pretty efficient system to identify ships with a heavy booty on board.
+
+Unfortunattely for you, people weigh a lot this days, so how do you know if a ship if full of gold and not people?
+
+You begin with writing a generic Ship class:
+
+function Ship(draft,crew) {
+ this.draft = draft;
+ this.crew = crew;
+}
+Every time your spies see a new ship enter the dock, they will create a new ship object based on their observations:
+
+draft - an estimate of the ship's weight based on how low it is in the water
+crew - the count of crew on board
+var titanic = new Ship(15, 10);
+Taking into account that an average crew member on board adds 1.5 units to the draft, a ship that has a draft of more than 20 without crew is considered worthy to loot. Any ship weighing that much must have a lot of booty!
+
+Add the method
+
+isWorthIt
+to decide if the ship is worthy to loot. For example:
+
+titanic.isWorthIt() // return false
+*/
+
+//Answer//
+
+function Ship(draft,crew) {
+ this.draft = draft;
+ this.crew = crew;
+}
+
+Ship.prototype.isWorthIt = function Ship(draft,crew) {
+return this.draft-(this.crew*1.5)>20
+}
