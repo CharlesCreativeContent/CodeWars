6e3829a21f19f6d6202e458e030be4d86dedc819
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-02 01:19:30 -0400

    Fix the functions closure problems
    192nd exercise - codewars.com/kata/514aa0dc21607ae236000017

---
+++ fixClosure.js
@@ -0,0 +1,17 @@
+/*
+Functional closures can get overly attached. Set them straight!
+
+Why doesn't greet_abe() actually greet Abe?
+*/
+
+//Answer//
+
+var greet_abe = function() {
+var name = 'Abe';
+return "Hello, " + name + '!';
+};
+
+var greet_ben = function() {
+var name = 'Ben';
+return "Hello, " + name + '!';
+};
