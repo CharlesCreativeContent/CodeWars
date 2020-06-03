d1f89659f47d53293c113edb99409c945f957c46
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-03 14:29:31 -0400

    Calculates how much water thegarden needs
    194th exercise - https://www.codewars.com/kata/57158fb92ad763bb180004e7

---
+++ codeGarden.js
@@ -0,0 +1,16 @@
+*
+You have an award-winning garden and everyday the plants need exactly 40mm of water. You created a great piece of JavaScript to calculate the amount of water your plants will need when you have taken into consideration the amount of rain water that is forecast for the day. Your jealous neighbour hacked your computer and filled your code with bugs.
+
+Your task is to debug the code before your plants die!
+*/
+
+//Answer//
+
+function rainAmount(mm){
+    if (mm < 40) {
+         return "You need to give your plant " + (40-mm) + "mm of water"
+    }
+    else {
+         return "Your plant has had more than enough water for today!"
+    };
+}
