4864da9cfc257d115ac2d8815f9675c54dda10a7
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-03 14:38:47 -0400

    Moves zeros to the end of the array
    1st exercise - https://www.codewars.com/kata/52597aa56021e91c93000cb0

---
+++ 5th Kyu/zerosToTheEnd.js
@@ -0,0 +1,12 @@
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+//Answer//
+
+var moveZeros = function (arr) {
+return arr.filter(x=>x!==0).concat(arr.filter(x=>x===0))
+
+}
