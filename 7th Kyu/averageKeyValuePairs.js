7a0ccaaed146679c86309c1ec75ae4fd8c960800
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-05 23:28:29 -0400

    Returns the average of all the key value pairs
    118th exercise - codewars.com/kata/57d2807295497e652b000139

---
+++ 7th Kyu/averageKeyValuePairs.js
@@ -0,0 +1,17 @@
+/*
+#Get the averages of these numbers
+
+Write a method, that gets an array of integer-numbers and return an array of the averages of each integer-number and his follower, if there is one.
+
+Example:
+
+Input:  [ 1, 3, 5, 1, -10]
+Output:  [ 2, 4, 3, -4.5]
+If the array has 0 or 1 values or is null or None, your method should return an empty array.
+*/
+
+//Answer//
+
+function averages(n) {
+return n===null?[]:n.slice(1).map((x,i)=>(x+n[i])/2);
+}
