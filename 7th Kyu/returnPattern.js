c58f49b7b5c94c40e6b95f6b185ee923c0b0d188
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-05 23:15:26 -0400

    Returns a Pattern of Numbered Strings
    116th exercise - codewars.com/kata/5572f7c346eb58ae9c000047

---
+++ 7th Kyu/returnPattern.js
@@ -0,0 +1,43 @@
+/*
+You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+
+Note:Returning the pattern is not the same as Printing the pattern.
+Rules/Note:
+If n < 1 then it should return "" i.e. empty string.
+There are no whitespaces in the pattern.
+Pattern:
+1
+22
+333
+....
+.....
+nnnnnn
+Examples:
+pattern(5):
+
+1
+22
+333
+4444
+55555
+pattern(11):
+
+1
+22
+333
+4444
+55555
+666666
+7777777
+88888888
+999999999
+10101010101010101010
+1111111111111111111111
+Hint: Use \n in string to jump to next line
+*/
+
+//Answer//
+
+function pattern(n){
+return n<1? '': [...Array(n+1).keys()].slice(1).map(x=>String(x).repeat(x)).join('\n')
+}
