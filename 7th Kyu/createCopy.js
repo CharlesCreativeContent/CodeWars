efa32576f41c39bece1515522a6e67c8f4b95ff8
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-05 23:22:37 -0400

    Creates a copy of an array
    117th exercise - codewars.com/kata/53d2697b7152a5e13d000b82

---
+++ 7th Kyu/createCopy.js
@@ -0,0 +1,15 @@
+/*
+Write a function that takes a list (in Python) or array (in other languages) of numbers, and makes a copy of it.
+
+Note that you may have troubles if you do not return an actual copy, item by item, just a pointer or an alias for an existing list or array.
+
+If not a list or array is given as a parameter in interpreted languages, the function should raise an error.
+
+*/
+
+//Answer//
+
+function copyList(l){
+let L = [...l]
+return L
+}
