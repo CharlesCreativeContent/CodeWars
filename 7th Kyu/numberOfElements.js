5d38c873b308b504fdbc4fd0edf880bd7ffbefbc
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-06 09:14:33 -0400

    Returns number of elements
    124th exercise - codewars.com/kata/52829c5fe08baf7edc00122b

---
+++ 7th Kyu/numberOfElements.js
@@ -0,0 +1,16 @@
+/*
+Write a function that returns the number of occurrences of an element in an array.
+
+Examples
+var arr = [0, 1, 2, 2, 3];
+arr.numberOfOccurrences(0) === 1;
+arr.numberOfOccurrences(4) === 0;
+arr.numberOfOccurrences(2) === 2;
+arr.numberOfOccurrences("a") === 0;
+*/
+
+//Answer//
+
+Array.prototype.numberOfOccurrences = function(num) {
+return this.filter(x=>x===num).length
+}
