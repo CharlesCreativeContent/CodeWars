d4a381dbcf94318506347e36c2e6f09aa0412ec9
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-06 09:13:51 -0400

    returns true only if one of the parameters is true
    123rd exercise - https://www.codewars.com/kata/54599705cbae2aa60b0011a4

---
+++ 7th Kyu/exactlyOne.js
@@ -0,0 +1,18 @@
+/*
+Create a function called one that accepts two params:
+
+a sequence
+a function
+and returns true only if the function in the params returns true for exactly one (1) item in the sequence.
+
+Example
+one([1, 3, 5, 6, 99, 1, 3], bigger_than_ten) -> true
+one([1, 3, 5, 6, 99, 88, 3], bigger_than_ten) -> false
+one([1, 3, 5, 6, 5, 1, 3], bigger_than_ten) -> false
+*/
+
+//Answer//
+
+function one(arr, fun){
+return arr.filter(fun).length===1
+}
