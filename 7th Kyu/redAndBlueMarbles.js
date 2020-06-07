07e6139ef7a707b343c4d7316fec2ad164447a43
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-07 16:20:39 -0400

    Counts red marbles
    127th exercise - https://www.codewars.com/kata/58712dfa5c538b6fc7000569

---
+++ 7th Kyu/redAndBlueMarbles.js
@@ -0,0 +1,14 @@
+/*
+Two red beads are placed between every two blue beads. There are N blue beads. After looking at the arrangement below work out the number of red beads.
+
+@ @@ @ @@ @ @@ @ @@ @ @@ @
+
+Implement count_red_beads(n) (in PHP count_red_beads($n); in Java, Javascript, TypeScript, C, C++ countRedBeads(n)) so that it returns the number of red beads.
+If there are less than 2 blue beads return 0.
+*/
+
+//Answer//
+
+function countRedBeads(n) {
+return n<2?0:(n-1)*2
+}
