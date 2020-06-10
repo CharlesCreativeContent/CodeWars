df85d910cac41e9589e4ce7c869e55e2fa9c9869
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-10 15:08:48 -0400

    Calculates the total score of local team
    195th exercise - codewars.com/kata/5bb904724c47249b10000131

---
+++ totalScore.js
@@ -0,0 +1,28 @@
+/*
+Our football team finished the championship. The result of each match look like "x:y". Results of all matches are recorded in the collection.
+
+For example: ["3:1", "2:2", "0:1", ...]
+
+Write a function that takes such collection and counts the points of our team in the championship. Rules for counting points for each match:
+
+if x>y - 3 points
+if x<y - 0 point
+if x=y - 1 point
+Notes:
+
+there are 10 matches in the championship
+0 <= x <= 4
+0 <= y <= 4
+*/
+
+//Answer//
+
+const points=games=>games.reduce((output,current)=>{
+    return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+  },0)
+  
+//OR//
+
+function points(games) {
+return games.map(x=>+x[0]-(+x[2])).map(x=>{if(x===0){return 1}else if (x<0){return 0}else{return 3}}).reduce((a,b)=>a+b)
+}
