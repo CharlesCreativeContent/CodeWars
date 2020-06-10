ac6ce2853c49c86369920b8ef1575f0cd8b86699
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-10 15:05:34 -0400

    Four functions that slice an array
    135th exercise - codewars.com/kata/54592a5052756d5c5d0009c3

---
+++ 7th Kyu/headTail.js
@@ -0,0 +1,46 @@
+/*
+Haskell has some useful functions for dealing with lists:
+
+$ ghci
+GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+λ head [1,2,3,4,5]
+1
+λ tail [1,2,3,4,5]
+[2,3,4,5]
+λ init [1,2,3,4,5]
+[1,2,3,4]
+λ last [1,2,3,4,5]
+5
+Your job is to implement these functions in your given language. Make sure it doesn't edit the array; that would cause problems! Here's a cheat sheet:
+
+| HEAD | <----------- TAIL ------------> |
+[  1,  2,  3,  4,  5,  6,  7,  8,  9,  10]
+| <----------- INIT ------------> | LAST |
+
+head [x] = x
+tail [x] = []
+init [x] = []
+last [x] = x
+Here's how I expect the functions to be called in your language:
+
+head([1,2,3,4,5]); => 1
+tail([1,2,3,4,5]); => [2,3,4,5]
+Most tests consist of 100 randomly generated arrays, each with four tests, one for each operation. There are 400 tests overall. No empty arrays will be given. Haskell has QuickCheck tests
+*/
+
+//Answer//
+
+function head(a) {return a[0]}
+
+function last(a) {return a[a.length - 1]}
+
+function init(a) {return a.slice(0, -1)}
+
+function tail(a) {return a.slice(1)}
+
+//OR//
+
+head=x=>x[0]
+tail=x=>x.slice(1)
+init=x=>x.slice(0,x.length-1)
+last=x=>x[x.length-1]
