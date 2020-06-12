723b71ea22d87869ee0defe59ac8d989af183d60
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-12 11:05:01 -0400

    Returns the nth even number
    199th exercise - codewars.com/kata/5933a1f8552bc2750a0000ed

---
+++ nthEven.js
@@ -0,0 +1,16 @@
+/*
+Return the Nth Even Number
+
+nthEven(1) //=> 0, the first even number is 0
+nthEven(3) //=> 4, the 3rd even number is 4 (0, 2, 4)
+
+nthEven(100) //=> 198
+nthEven(1298734) //=> 2597466
+The input will not be 0.
+*/
+
+//Answer//
+
+function nthEven(n){
+return (n*2)-2
+}
