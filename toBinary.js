6c23c64f1d2ddce29af5ad6e0d1714dc1749ac2b
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-13 00:14:38 -0400

    Returns number in binary
    201st exercise - codewars.com/kata/59fca81a5712f9fa4700159a

---
+++ toBinary.js
@@ -0,0 +1,18 @@
+/*
+Given a non-negative integer n, write a function toBinary/ToBinary which returns that number in a binary format.
+
+to_binary(1)  /* should return 1 */
+to_binary(5)  /* should return 101 */
+to_binary(11) /* should return 1011 */
+Example:
+
+to_binary(1)  /* should return 1 */
+to_binary(5)  /* should return 101 */
+to_binary(11) /* should return 1011 */
+*/
+
+//Answer//
+
+function toBinary(n){
+  return Number(n.toString(2));
+}
