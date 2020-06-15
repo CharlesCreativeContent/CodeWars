f07a4717e87255e815de3aa58affab9bb2165162
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-15 11:49:22 -0400

    Accomplished the kata objectives
    213th exercise - codewars.com/kata/571ec274b1c8d4a61c0000c8

---
+++ missionObjectives.js
@@ -0,0 +1,22 @@
+/*
+Please refer to two example above and write your first JS function.
+
+mission 1:
+
+use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+
+mission 2:
+
+use keyword var to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+
+mission 3:
+
+type the console.log() in the next line(don't forget to put the str in the brackets).
+*/
+
+//Answer//
+
+helloWorld=()=>{
+var str='Hello World!';
+console.log(str)
+}
