404672f0116113da53a3a96c35eb92b2665a0c11
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-17 00:12:09 -0400

    Returns new position of hero character
    223rd exercise - codewars.com/kata/563a631f7cbbc236cf0000c2

---
+++ moveHero.js
@@ -0,0 +1,14 @@
+    /*
+    In this game, the hero moves from left to right. The player rolls the dice and moves the number of spaces indicated by the dice two times.
+
+    Create a function for the terminal game that takes the current position of the hero and the roll (1-6) and return the new position.
+
+    Example:
+    move(3, 6) should equal 15
+    */
+
+    //Answer//
+
+    function move (p, r) {
+    return p + (2*r)
+    }
