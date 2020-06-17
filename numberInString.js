71a6fc956148996cc4f1dc5484d12e862fae4807
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-17 00:13:34 -0400

    Returns the numbers in a string
    224th exercise -https://www.codewars.com/kata/57a37f3cbb99449513000cd8

---
+++ numberInString.js
@@ -0,0 +1,19 @@
+/*
+Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+Function: ####javascript
+
+getNumberFromString(s)
+####ruby
+
+get_number_from_string(s)
+####CSharp
+
+GetNumberFromString(string s)
+*/
+
+//Answer//
+
+function getNumberFromString(s) {
+return +s.replace(/\D/g,'')
+}
