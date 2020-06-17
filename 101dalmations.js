cd66684069b7211ec74c652e7ddde68d8701c450
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-17 00:10:32 -0400

    Gives reply to dog count
    220th exercise - https://www.codewars.com/kata/56f6919a6b88de18ff000b36

---
+++ 101dalmations.js
@@ -0,0 +1,17 @@
+/*
+Your friend has been out shopping for puppies (what a time to be alive!)... He arrives back with multiple dogs, and you simply do not know how to respond!
+
+By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
+
+The number of dogs will always be a number and there will always be at least 1 dog.
+*/
+
+//Answer//
+
+function howManyDalmatians(n)
+ { 
+  var dogs = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
+  
+return n <= 10 ? dogs[0] : n <= 50 ? dogs[1] : n == 101 ? dogs[3] : dogs[2]
+
+}'
