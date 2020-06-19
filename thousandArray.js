1509404237f0657db9c29b5440c21c299329311f
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-19 14:18:58 -0400

    Creates an array with a thousand elements
    233rd exercise - https://www.codewars.com/kata/525c1a07bb6dda6944000031

---
+++ thousandArray.js
@@ -0,0 +1,11 @@
+/*
+This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+Add the value "codewars" to the array websites/Websites 1,000 times.
+
+var websites = []
+*/
+
+//Answer//
+
+var websites = Array(1000).fill('codewars')
