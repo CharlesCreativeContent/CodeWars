55fc4c3f835bb2db99f27c5f73f0c8c077dd0d61
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-19 14:17:52 -0400

    Updates an element of an array
    232nd exercise - codewars.com/kata/511f0fe64ae8683297000001

---
+++ updateArray.js
@@ -0,0 +1,12 @@
+/*
+Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"]
+
+The websites array has already been defined for you using the following code:
+
+var websites = [];
+*/
+
+//Answer//
+
+websites[0]=("codewars")
