90f8c37a3dd9901a855a3daff569ea26b43999b1
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-20 08:32:33 -0400

    Pushes an object into an array
    239th exercise - codewars.com/kata/527b3cd0492b6b15250060af

---
+++ pushHash.js
@@ -0,0 +1,8 @@
+/*
+You are trying to put a hash in ruby or an object in javascript or java into an array, but it always returns error, solve it and keep it as simple as possible!
+*/
+
+//Answer//
+
+items = []
+items.push({a: "b", c: "d"})
