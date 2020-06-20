c1b3b93031e6521e0f087dbd2cc3703451478833
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-20 08:28:45 -0400

    Fixes another bug
    237th exercise - codewars.com/kata/53f9ee9f64b19d4b1d0001ed

---
+++ fixAnotherBug.js
@@ -0,0 +1,26 @@
+/*
+VVhat ?!?
+
+None of zese codevarriors seemz to remember hiz ovvn name !
+
+Kould you help ?
+
+var albert = new Warrior("Al")
+var boris  = new Warrior("Boris")
+
+albert.toString() --> "Hi! my name's Boris" <-- ohoh..
+*/
+
+//Answer//
+
+function Warrior(n){
+ let name = n;  
+  this.name = function(n){
+    if( n ) name=n;
+    return name;
+  }
+}
+  
+Warrior.prototype.toString = function(){
+    return "Hi! my name's "+this.name();
+}
