f475710927797d63737163fe75f7d0133ca7408b
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-20 08:31:31 -0400

    Creates another max function
    238th exercise - codewars.com/kata/52a47dd7e950edabfa000391

---
+++ anotherMax.js
@@ -0,0 +1,25 @@
+/*
+Shouldn't the two functions getMax1 and getMax2 be equivalent and return the same value? Can you spot a problem and fix it? Can you learn something about JavaScript style in this kata?
+
+
+*/
+
+//Answer//
+
+function getMax1()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
