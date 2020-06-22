589b84da78ffca51fc1aa5542b629d898720a0f4
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-22 10:37:40 -0400

    Returns a template string
    247th exercise - codewars.com/kata/55a14f75ceda999ced000048

---
+++ templateStrings.js
@@ -0,0 +1,13 @@
+/*
+Your task is to return the correct string using the Template String Feature.
+Input
+Two Strings, no validation is needed.
+Output
+You must output a string containing the two strings with the word ```' are '```
+*/
+
+//Answer//
+
+var TempleStrings = function(obj, feature) {
+  return `${obj} are ${feature}`
+}
