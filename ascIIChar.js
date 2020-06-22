ba5038f1a0c51afd35c460cbc58f04806754fd54
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-22 10:42:37 -0400

    Returns the ascII character for the input number
    249th exercise - codewars.com/kata/55ad04714f0b468e8200001c

---
+++ ascIIChar.js
@@ -0,0 +1,13 @@
+/*
+Write a function which takes a number and returns the corresponding ASCII char for that value.
+
+Example:
+
+get_char(65) # => 'A'
+*/
+
+//Answer//
+
+function getChar(c){
+return String.fromCharCode(c)
+}
