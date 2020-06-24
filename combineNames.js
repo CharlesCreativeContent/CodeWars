163318c7bf624489b415952fb3406a8f548d954b
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-24 13:49:12 -0400

    Returns full name
    257th exercise - codewars.com/kata/55f73f66d160f1f1db000059

---
+++ combineNames.js
@@ -0,0 +1,14 @@
+/*
+Create a function named combineNames(combine_names in python, ruby, CombineNames in C#) that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+*/
+
+//Answer//
+
+let combineNames = (a,b)=>a+' '+b
