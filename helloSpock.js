4f1d89911708133b7c03373833485003a4b83598
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-06-24 13:48:18 -0400

    Greeting system for starship Enterprise
    256th exercise - codewars.com/kata/5625618b1fe21ab49f00001f

---
+++ helloSpock.js
@@ -0,0 +1,11 @@
+/*
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+
+Example output:
+
+Hello, Mr. Spock
+*/
+
+//Answer//
+
+let sayHello=(name)=> 'Hello, '+name
