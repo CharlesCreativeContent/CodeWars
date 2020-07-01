a7db14ee41c4b8468a3900e909e2d114bc14f4f3
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-01 10:41:35 -0400

    Verifies what quarter given the month
    276th exercise - https://www.codewars.com/kata/5ce9c1000bab0b001134f5af

---
+++ quarterOfYear.js
@@ -0,0 +1,11 @@
+/*
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+*/
+
+//Answer//
+
+const quarterOf = (month) => {
+ return Math.ceil(month/3)
+}
