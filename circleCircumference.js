d6dfffe4db578ce686c811aaf2b8d2535fc00296
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-03 09:14:47 -0400

    Returns the circumference of a circle object
    287th exercise - https://www.codewars.com/kata/58e43389acfd3e81d5000a88

---
+++ circleCircumference.js
@@ -0,0 +1,15 @@
+/*
+This series of katas will introduce you to basics of doing geometry with computers.
+
+Point objects have x, y attributes. Circle objects have center which is a Point, and radius, which is a number.
+
+Write a function calculating circumference of a Circle.
+
+Tests round answers to 6 decimal places.
+*/
+
+//Answer//
+
+function circleCircumference(circle) {
+return 2*Math.PI*circle.radius
+}
