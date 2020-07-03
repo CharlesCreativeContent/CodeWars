54c7b4de930980dae39041a20173805b1c093d44
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-02 21:57:12 -0400

    returns power operator
    283rd exercise - https://www.codewars.com/kata/59fc72fe235f93838b002235

---
+++ usingPowers.js
@@ -0,0 +1,15 @@
+/*
+The function takes in 2 inputs x and y, and should return x to the power of y
+
+Simple, right? But you're NOT allowed to use Math.pow();
+
+Obs: x and y will be naturals, so DON'T take fractions into consideration;
+
+Note : zero to the power of zero equals one in this kata
+*/
+
+//Answer//
+
+function power(x,y){
+return x**y
+}
