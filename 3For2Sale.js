515edfb681b91a522d9e4172c84c8bd6f0907945
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-05 14:03:10 -0400

    Calculates price of mangoes after discount
    291st exercise - codewars.com/kata/57a77726bb9944d000000b06

---
+++ 3For2Sale.js
@@ -0,0 +1,13 @@
+/*
+There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+
+Examples
+mango(3, 3) ==> 6    # 2 mangoes for 3 = 6; +1 mango for free
+mango(9, 5) ==> 30   # 6 mangoes for 5 = 30; +3 mangoes for free
+*/
+
+//Answer//
+
+function mango(q, p){
+return (q-Math.floor(q/3))*p
+}
