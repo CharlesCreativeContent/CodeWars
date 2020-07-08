1c290ac48d2c0ec845c06783731134a4b5df7502
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-08 03:33:15 -0400

    verifies if you have any undamaged arrows
    309th exercise - codewars.com/kata/559f860f8c0d6c7784000119

---
+++ damagedArrows.js
@@ -0,0 +1,13 @@
+/*
+You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
+You need to verify that you have some good ones left, in order to prepare for battle:
+
+anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}])
+If an arrow in the quiver does not have a damaged status, it means it's new.
+
+The expected result is a boolean, indicating whether you have any good arrows left.
+*/
+
+//Answer//
+
+let anyArrows = (arrows)=> arrows.some(x =>x.damaged!==true )
