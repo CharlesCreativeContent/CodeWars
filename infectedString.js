78dd7485d026896728c393df223e2d1132deba26
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-08 03:29:04 -0400

    Verifies if string is infected
    305th exercise - codewars.com/kata/596fba44963025c878000039

---
+++ infectedString.js
@@ -0,0 +1,19 @@
+/*
+An AI has infected a text with a character!!
+
+This text is now fully mutated to this character.
+
+If the text or the character are empty, return an empty string.
+There will never be a case when both are empty as nothing is going on!!
+
+Note: The character is a string of length 1 or an empty string.
+
+Example
+text before = "abc"
+character   = "z"
+text after  = "zzz"
+*/
+
+//Answer//
+
+let contamination = (text, char)=> char.repeat(text.length)
