74ce04ba16a188b073bef161c3ebb975a5283bc1
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-09 02:43:11 -0400

    Replaces dots with dashed
    312th exercise - https://www.codewars.com/kata/596c6eb85b0f515834000049

---
+++ replaceDots.js
@@ -0,0 +1,15 @@
+/*
+The code provided is supposed replace all the dots . in the specified String str with dashes -
+
+But it's not working properly.
+
+Task
+Fix the bug so we can all go home early.
+
+Notes
+String str will never be null.
+*/
+
+//Answer//
+
+let replaceDots = str=> str.replace(/\./g, '-')
