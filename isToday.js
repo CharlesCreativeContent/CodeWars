07aee16d08814cb1e7cb0fb2605ab97fcc5fd84f
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-10 09:43:40 -0400

    Verifies if date object is today
    316th exercise - https://www.codewars.com/kata/563c13853b07a8f17c000022

--- isToday.js
+++ isToday.js
@@ -7,28 +7,3 @@ Make sure that your function does not return a false positive by just checking j
 //Answer//
 
 let isToday = date => date.getDay()=== new Date().getDay()
-
-JavaScript:
-315th exercise - https://www.codewars.com/kata/56d8ae9237123036d3001b54
-/*
-While making a zork-type game, you create an object of rooms. Unfortunately, the game is not working. Find all of the errors in the rooms object to get your game working again.
-*/
-
-//Answer//
-
-var rooms = {
-  first: {
-    description: 'This is the first room',
-    items: {
-      chair: 'The old chair looks comfortable',
-      lamp: 'This lamp looks ancient'
-  }
-  },
-  second: {
-    description: 'This is the second room',
-    items: {
-      couch: 'This couch looks like it would hurt your back',
-      table: 'On the table there is an unopened bottle of water'
-    }
-  }
-}
