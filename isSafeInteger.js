bed3774c7225e209fb49cc9f1a56017c5a727992
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-12 00:21:48 -0400

    Verifies if val is a safe integer
    324th exercise - https://www.codewars.com/kata/55a4f9afeffe4231090000d6

---
+++ isSafeInteger.js
@@ -0,0 +1,16 @@
+/*
+Not all integers can be represented by JavaScript/TypeScript. It has space to to represent 53bit signed integers. In this Kata, we've to determine if it is safe to use the integer or not. Make use of the latest ES6 features to find this.
+
+SafeInteger(9007199254740990) //true
+SafeInteger(-90) //true
+SafeInteger(9007199254740992) //false
+*/
+
+//Answer//
+
+let SafeInteger = n => n<=9007199254740991
+
+//OR//
+
+SafeInteger = Number.isSafeInteger;
+
