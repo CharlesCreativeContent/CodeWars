a03a7d3dfa0419829774565df19f83d1d29aa727
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-12 00:24:56 -0400

    Verifies val that doesn't equal itself
    325th exercise - https://www.codewars.com/kata/560f8d41cf6e1fe5c900002e

---
+++ findStrangeValue.js
@@ -0,0 +1,12 @@
+/*
+In JavaScript, there is a special case where strict comparison of the same variable returns false! Try to find out what must be done to get such result!
+
+var x = something;
+x === x // returns false!
+*/
+
+//Answer//
+
+function ) {
+return NaN
+}
