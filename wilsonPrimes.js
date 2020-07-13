07f9cbccc9484fd500a55c7ef846877e79fe58ed
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-13 00:30:19 -0400

    Verifies if number is a wilson prime
    330th exercise - https://www.codewars.com/kata/55dc4520094bbaf50e0000cb

---
+++ wilsonPrimes.js
@@ -0,0 +1,22 @@
+/*
+Wilson primes satisfy the following condition. Let P represent a prime number.
+
+Then ((P-1)! + 1) / (P * P) should give a whole number.
+
+Your task is to create a function that returns true if the given number is a Wilson prime.
+*/
+
+//Answer//
+
+function amIWilson(P) {
+function factorialize(num) {
+  if (num < 0) 
+        return -1;
+  else if (num == 0) 
+      return 1;
+  else {
+      return (num * factorialize(num - 1));
+  }
+}
+return !(((factorialize(P-1) + 1)/(P * P))%1)
+}
