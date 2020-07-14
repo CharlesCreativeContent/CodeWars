e35972e53625736c43aa5fa231a124494b6ea1c7
Shawn Clearly <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-14 02:27:51 -0400

    Maximum number obtained through changing operators
    331st exercise - https://www.codewars.com/kata/5ae62fcf252e66d44d00008e

---
+++ expressionMatter.js
@@ -0,0 +1,15 @@
+/*
+Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+Consider an Example :
+With the numbers are 1, 2 and 3 , here are some ways of placing signs and brackets:
+
+1 * (2 + 3) = 5
+1 * 2 * 3 = 6
+1 + 2 * 3 = 7
+(1 + 2) * 3 = 9
+So the maximum value that you can obtain is 9.
+*/
+
+//Answer//
+let expressionMatter = (a, b, c) => Math.max(a+b+c,(a+b)*c,a*(b+c),a*b*c )
