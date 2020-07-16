0b45786b009578b861b8214e4742177d312665c9
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-15 23:36:53 -0400

    Removes n number of "!" from string
    336th exercise - https://www.codewars.com/kata/57faf7275c991027af000679

---
+++ removeExclamations.js
@@ -0,0 +1,28 @@
+*
+Remove n exclamation marks in the sentence from left to right. n is positive integer.
+
+Examples
+remove("Hi!",1) === "Hi"
+remove("Hi!",100) === "Hi"
+remove("Hi!!!",1) === "Hi!!"
+remove("Hi!!!",100) === "Hi"
+remove("!Hi",1) === "Hi"
+remove("!Hi!",1) === "Hi!"
+remove("!Hi!",100) === "Hi"
+remove("!!!Hi !!hi!!! !hi",1) === "!!Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",3) === "Hi !!hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",5) === "Hi hi!!! !hi"
+remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
+*/
+
+//Answer//
+
+function remove(s,n){
+let count = 0
+while(count!=n){
+  s=s.replace('!','')
+  count++
+}
+return s
+}
+
