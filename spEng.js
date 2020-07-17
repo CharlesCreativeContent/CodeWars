7787dce621560a14958b5c5f79a95512ff6d2c24
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-16 23:52:19 -0400

    Contains English?
    341st exercise - https://www.codewars.com/kata/58dbdccee5ee8fa2f9000058

---
+++ spEng.js
@@ -0,0 +1,13 @@
+/*
+Given a string of arbitrary length with any ascii characters. Write a function to determine whether the string contains the whole word "English".
+
+The order of characters is important -- a string "abcEnglishdef" is correct but "abcnEglishsef" is not correct.
+
+Upper or lower case letter does not matter -- "eNglisH" is also correct.
+
+Return value as boolean values, true for the string to contains "English", false for it does not.
+*/
+
+//Answer//
+
+let spEng = s => s.search(/(english)/i)>=0
