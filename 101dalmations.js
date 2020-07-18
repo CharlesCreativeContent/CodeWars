a21ff3c4f8ef9f492a4cb647b69f5029088a367d
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-07-17 23:15:07 -0400

    Update 101dalmations.js

--- 101dalmations.js
+++ 101dalmations.js
@@ -4,7 +4,7 @@ Your friend has been out shopping for puppies (what a time to be alive!)... He a
 By repairing the function provided, you will find out exactly how you should respond, depending on the number of dogs he has.
 
 The number of dogs will always be a number and there will always be at least 1 dog.
-*/
+*/ 
 
 //Answer//
 
