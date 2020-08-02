f73c0bbd46e653d8c776102dd3e565a7ef18f3eb
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-08-01 23:53:38 -0400

    Update welcomeLanguages.js

--- welcomeLanguages.js
+++ welcomeLanguages.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries.
 Marketing thinks it would be great to welcome visitors to the site in their own language.
 Luckily you already use an API that detects the user's location, so this is an easy win.
