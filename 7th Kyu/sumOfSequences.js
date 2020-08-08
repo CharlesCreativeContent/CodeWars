6db2b75e95e5f56b5dd3246f60d7ccbcebbef095
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-08-07 23:39:48 -0400

    Update sumOfSequences.js

--- 7th Kyu/sumOfSequences.js
+++ 7th Kyu/sumOfSequences.js
@@ -1,6 +1,6 @@
 /*
 Your task is to make function, which returns the sum of a sequence of integers.
-
+ 
 The sequence is defined by 3 non-negative values: begin, end, step.
 
 If begin value is greater than the end, function should return 0
