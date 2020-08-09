91202a8cb832107c089846ed20b5970dfe88dabe
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-08-09 17:28:07 -0400

    Update narcissisticNumber.js

--- 6th Kyu/narcissisticNumber.js
+++ 6th Kyu/narcissisticNumber.js
@@ -1,6 +1,6 @@
 !ans
 
 function narcissistic(value) {
-if(value.toString().split('').map(x=>x**value.toString().length).reduce((a, b) => a + b)===value){ 
+if(value.toString().split('').map(x=>x**value.toString().length).reduce((a, b) => a + b)===value){
 return true}else{return false}
 }
