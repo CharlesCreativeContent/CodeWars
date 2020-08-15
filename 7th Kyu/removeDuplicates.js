268646ded690f451af4bfe602e78c8c9690f22cd
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-08-14 22:24:54 -0400

    Create removeDuplicates.js
    codewars.com/kata/5ba38ba180824a86850000f7

---
+++ 7th Kyu/removeDuplicates.js
@@ -0,0 +1,14 @@
+/*
+In this Kata, you will remove the left-most duplicates from a list of integers and return the result.
+
+// Remove the 3's at indices 0 and 3
+// followed by removing a 4 at index 1
+solve([3, 4, 4, 3, 6, 3]); // => [4, 6, 3]
+More examples can be found in the test cases.
+*/
+
+//Answer//
+
+function solve(arr){
+return [...new Set(arr.reverse())].reverse()
+}
