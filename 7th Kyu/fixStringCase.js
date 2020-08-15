de63afe9266f5b392867d15eb004d307d8e5def2
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-08-14 22:27:43 -0400

    Create fixStringCase.js
    codewars.com/kata/5b180e9fedaa564a7000009a

---
+++ 7th Kyu/fixStringCase.js
@@ -0,0 +1,15 @@
+/*
+In this Kata, you will be given a string that may have mixed uppercase and lowercase letters and your task is to convert that string to either lowercase only or uppercase only based on:
+
+make as few changes as possible.
+if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+For example:
+
+solve("coDe") = "code". Lowercase characters > uppercase. Change only the "D" to lowercase.
+solve("CODe") = "CODE". Uppercase characters > lowecase. Change only the "e" to uppercase.
+solve("coDE") = "code". Upper == lowercase. Change all to lowercase.
+*/
+
+//Answer//
+
+const solve=s=>s.split('').filter(x=>x.toUpperCase()==x).length<=s.length/2?s.toLowerCase():s.toUpperCase()
