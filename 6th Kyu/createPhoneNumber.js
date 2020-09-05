00b27015809a54467d4cf8c8f83e76327e067a21
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-09-04 23:52:56 -0400

    Update createPhoneNumber.js

--- 6th Kyu/createPhoneNumber.js
+++ 6th Kyu/createPhoneNumber.js
@@ -15,4 +15,4 @@ function createPhoneNumber(numbers){
 
 function createPhoneNumber(numbers){
   return `(${numbers.slice(0,3).join('')}) ${numbers.slice(3,6).join('')}-${numbers.slice(6).join('')}`
-}
+} 
