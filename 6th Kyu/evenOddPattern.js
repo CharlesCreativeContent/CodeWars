3915ad1241ad1f667d792481c0cfe193a3d89243
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-09-20 18:02:02 -0400

    Create evenOddPattern.js
    https://www.codewars.com/kata/559e708e72d342b0c900007b

---
+++ 6th Kyu/evenOddPattern.js
@@ -0,0 +1,15 @@
+
+/*
+Write a function that takes an array/list of numbers and returns a number.
+
+See the examples and try to guess the pattern:
+
+EvenOdd([1,2,6,1,6,3,1,9,6]) => 393
+EvenOdd([1,2,3]) => 5
+EvenOdd([0,2,3]) => 3
+EvenOdd([1,0,3]) => 3
+EvenOdd([3,2])   => 6
+*/
+
+//Answer//
+const EvenOdd = a => a.reduce((a,b,i)=>!(i%2)?a+b:a*b,0)
