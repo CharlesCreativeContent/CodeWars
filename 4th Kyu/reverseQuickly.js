58b55b11ae8167d56f503c9ad1eb567bc1665b85
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-11-05 00:14:49 -0500

    Shortest way to reverse Array
    https://www.codewars.com/kata/59ae589c07157afba80000a7 - 1st exercise

---
+++ 4th Kyu/reverseQuickly.js
@@ -0,0 +1,10 @@
+/*
+Typically, reversing an array is a pretty straightforward task even for novice programmers. But not when a crowd of angry zombies scratching your door, looking for a fresh brains. In this case even skilled ninja-geek probably prefer to quickly push his code on github to have enough time to find a chainsaw. So there's two obstacles:
+
+Your code needs to be as short as possible, in fact not longer than 28 characters
+Because you are scared and stressed you have forgotten how to use the standard reverse() method
+#Input: an array containing data of any types. Ex: [1,2,3,'a','b','c',[]] #Output: [[],'c','b','a',3,2,1]
+*/
+
+/*Answer*/
+weirdReverse=a=>a.sort(a=>8)
