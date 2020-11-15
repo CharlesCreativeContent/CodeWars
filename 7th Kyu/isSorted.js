fab26b04c34cb9e31c7aed355a44d9f788a8dfcd
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2020-11-15 00:38:46 -0500

    Update isSorted.js

--- 7th Kyu/isSorted.js
+++ 7th Kyu/isSorted.js
@@ -2,7 +2,7 @@
 In this Kata, your function receives an array of integers as input. Your task is to determine whether the numbers are in ascending order. An array is said to be in ascending order if there are no two adjacent integers where the left integer exceeds the right integer in value.
 
 For the purposes of this Kata, you may assume that all inputs are valid, i.e. non-empty arrays containing only integers.
-
+ 
 Note that an array of 1 integer is automatically considered to be sorted in ascending order since all (zero) adjacent pairs of integers satisfy the condition that the left integer does not exceed the right integer in value. An empty list is considered a degenerate case and therefore will not be tested in this Kata - feel free to raise an Issue if you see such a list being tested.
 
 For example:
